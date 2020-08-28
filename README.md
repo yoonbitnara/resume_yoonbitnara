e17bc391544308bb71adb9ee37d840451a9e7dab
jin sun kyoung <jinsk9268@gmail.com>
2020-08-29 01:44:48 +0900

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-## `Resume YOON BITNARA`
-
-리액트로 만든 자기소개 페이지입니다
---
+++ precache-manifest.7968092be0d8bd1f7c8441d5ac42213b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a4538f20becdb1d6b830b3d63cb53f",
+    "url": "/resume_yoonbitnara/index.html"
+  },
+  {
+    "revision": "259f8d105cbe055af812",
+    "url": "/resume_yoonbitnara/static/css/main.44e43300.chunk.css"
+  },
+  {
+    "revision": "9eaa33c040c3583b3466",
+    "url": "/resume_yoonbitnara/static/js/2.d8d7afc5.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/resume_yoonbitnara/static/js/2.d8d7afc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "259f8d105cbe055af812",
+    "url": "/resume_yoonbitnara/static/js/main.8307ba35.chunk.js"
+  },
+  {
+    "revision": "c90abf3d7085fa7c74d1",
+    "url": "/resume_yoonbitnara/static/js/runtime-main.128ddd36.js"
+  },
+  {
+    "revision": "7e0a26bd80024e5d2abb1e24cf2b0e82",
+    "url": "/resume_yoonbitnara/static/media/YOON BITNARA.7e0a26bd.png"
+  }
+]);
